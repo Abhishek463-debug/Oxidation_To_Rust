1c0cdefb236c0cf51467a63955a1957458e8f970
Abhishek <91598690+Abhishek463-debug@users.noreply.github.com>
2022-07-22 16:16:11 +0530

    Add files via upload

--- nosuchthingaswastedtime.rs
+++ nosuchthingaswastedtime.rs
@@ -1,3 +1,5 @@
 pub fn print_something(){
     println!("It's never too late to change your life! It finally begins!");
+    println!("I love pizza, chicken wings, cheese, and vodka, and junk food, and burgers and fries and basically all good fooddddd..... :D");
+    println!("Let's play yugioh and learn magic!");
 }
