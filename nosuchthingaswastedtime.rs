c65ae9a9420218491a8c542d74990995deaaf80b
Abhishek <91598690+Abhishek463-debug@users.noreply.github.com>
2022-07-22 18:12:49 +0530

    Update nosuchthingaswastedtime.rs

--- nosuchthingaswastedtime.rs
+++ nosuchthingaswastedtime.rs
@@ -2,4 +2,5 @@ pub fn print_something(){
     println!("It's never too late to change your life! It finally begins!");
     println!("I love pizza, chicken wings, cheese, and vodka, and junk food, and burgers and fries and basically all good fooddddd..... :D");
     println!("Let's play yugioh and learn magic!");
+    println!("Dope people think war is cool :)");
 }
